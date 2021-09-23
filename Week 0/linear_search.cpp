08b563a15b09a9575c8d1244b82946afcdbbae75
Ayush0084 <72147777+Ayush0084@users.noreply.github.com>
2021-09-23 18:40:14 +0530

    Add files via upload

---
+++ Week 0/linear_search.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n,t,x,count,flag;
+    in >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        in >> arr[i];
+    }
+    in >> t;
+    while (t--)
+    {
+        in >> x;
+        count = flag = 0;
+        for (int i = 0; i < n; i++)
+        {
+            count++;
+            if(x == arr[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag)
+            out << "Found after " << count << " comparisons" << endl;
+        else
+            out << "Not Found" << endl;
+    }
+    in.close();
+    out.close();
+    return 0;
+}
